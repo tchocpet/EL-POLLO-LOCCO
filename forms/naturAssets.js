b6c8fc84e7c7e447a5f5571c383825ebd5f02bb7
Thierno Diallo <thiernomam78@gmail.com>
2026-04-27 19:31:55 +0200

    screen fix

--- forms/naturAssets.js
+++
@@ -1,5 +0,0 @@
-// Dummy-Implementierung, damit der Fehler verschwindet
-function loadNaturImages(natur) {
-  // Hier könnten Bilder geladen werden, aktuell nur Platzhalter
-  natur.images.loaded = true;
-}
